d0ea191d04546edb0ce5613376447a6075b7a1c5
Mukotso <kelvinmukotso@gmail.com>
2022-01-14 10:19:40 +0300

    express commit

--- books-api.js
+++ books-api.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json())
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Where we will keep books
 let books = [];
